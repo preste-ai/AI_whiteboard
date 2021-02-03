df0793f6049403cac1cefd0fa9d01407634086a0
preste-nakam <nazar.kaminskyi@preste.ai>
2021-02-03 12:03:17 +0200

    update gst_camera input patameters

--- gst_cam.py
+++ gst_cam.py
@@ -19,8 +19,7 @@ def gstreamer_pipeline(
     capture_height=720,
     display_width=1280,
     display_height=720,
-    framerate=60,
-    flip_method=0,
+    framerate=60
 ):
     return (
         "nvarguscamerasrc ! "
@@ -35,7 +34,6 @@ def gstreamer_pipeline(
             capture_width,
             capture_height,
             framerate,
-            flip_method,
             display_width,
             display_height,
         )
@@ -44,8 +42,8 @@ def gstreamer_pipeline(
 
 def show_camera():
     # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
-    print(gstreamer_pipeline(flip_method=0))
-    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    print(gstreamer_pipeline())
+    cap = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
     if cap.isOpened():
         window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
         # Window
